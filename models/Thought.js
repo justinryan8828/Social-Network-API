92409faf880e27d1298c0b398caf62f57ac2114b
Justin Moore <justinmoore@Justins-MacBook-Air.local>
2023-08-22 16:03:12 -0400

    fixed thought modules js

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    reactions: [
+      reactionSchema
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
