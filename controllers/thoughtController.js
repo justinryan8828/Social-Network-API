24741241ba17a5b0f2e4de8c0cfcad0ad78556f9
Justin Moore <justinmoore@Justins-MacBook-Air.local>
2023-08-23 20:27:21 -0400

    fixed errors

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -52,4 +52,31 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    ).then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with this id!' })
+        : res.json(thought)
+    )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.body.reactionId } } },
+      { runValidators: true, new: true }
+    ).then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with this id!' })
+        : res.json(thought)
+    )
+      .catch((err) => res.status(500).json(err));
+  }
+
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -82,6 +82,22 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  // update user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
   // Remove friend from user
   removeFriend(req, res) {
     User.findOneAndUpdate(
